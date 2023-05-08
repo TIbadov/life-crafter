ceaf335e9f761ab04406605af2d722aee7c11ac9
Timur Ibadov <ibadov.timur@gmail.com>
2023-05-08 20:58:46 +0300

    Add long press processing

--- app/src/main/java/com/tibadov/lifecrafter/LifeCounter.kt
+++ app/src/main/java/com/tibadov/lifecrafter/LifeCounter.kt
@@ -1,6 +1,6 @@
 package com.tibadov.lifecrafter
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -9,13 +9,19 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 @Composable
 fun LifeCounter(count: MutableState<Int>, modifier: Modifier = Modifier) {
@@ -45,16 +51,36 @@ private fun CounterButton(
     isLeft: Boolean,
     count: MutableState<Int>
 ) {
+    val buttonOffset = 24.dp
+    val fontSize = 36.sp
+    val coroutineScope = rememberCoroutineScope()
+    val isLongPressActive = remember { mutableStateOf(false) }
+    val countUpdateFunc = { if (isLeft) count.value-- else count.value++ }
+
     Box(
         modifier = Modifier
             .fillMaxWidth(if (isLeft) 0.5f else 1.0f)
             .fillMaxHeight()
-            .clickable { if (isLeft) count.value-- else count.value++ }
+            .pointerInput(Unit) {
+                detectTapGestures(
+                    onTap = {
+                        countUpdateFunc()
+                    },
+                    onLongPress = { offset ->
+                        isLongPressActive.value = true
+                    },
+                    onPress = {
+                        tryAwaitRelease()
+                        if (isLongPressActive.value) {
+                            isLongPressActive.value = false
+                        }
+                    },
+                )
+            }
     ) {
-        var buttonOffset = 24.dp
         Text(
             text = if (isLeft) "-" else "+",
-            style = TextStyle(fontSize = 36.sp, fontWeight = FontWeight.Bold),
+            style = TextStyle(fontSize = fontSize, fontWeight = FontWeight.Bold),
             modifier = Modifier
                 .align(if (isLeft) Alignment.CenterStart else Alignment.CenterEnd)
                 .padding(
@@ -63,4 +89,12 @@ private fun CounterButton(
                 )
         )
     }
+
+    LaunchedEffect(isLongPressActive.value) {
+        while (isLongPressActive.value) {
+            delay(100)
+            countUpdateFunc()
+        }
+    }
+
 }
