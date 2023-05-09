809c59e3d169c3f4a304d5a06bf97d20e5bdc0ef
Timur Ibadov <ibadov.timur@gmail.com>
2023-05-09 15:23:09 +0300

    temp5

--- app/src/main/java/com/tibadov/lifecrafter/ControlPanel.kt
+++ app/src/main/java/com/tibadov/lifecrafter/ControlPanel.kt
@@ -1,29 +1,52 @@
 package com.tibadov.lifecrafter
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
-fun ControlPanel(navController: NavController, modifier: Modifier = Modifier) {
+fun ControlPanel(navController: NavController, onResetTap: () -> Unit, modifier: Modifier = Modifier) {
     Row(modifier = modifier
         .fillMaxWidth()
-        .background(color = Color(0x55FF0000))
     ) {
-        IconButton(onClick = { println("^^^ Reset!") }) {
-            Icon(Icons.Default.Refresh, contentDescription = "Settings")
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onLongPress = {
+                            println("^^^ Reset!")
+                        },
+                        onTap = {
+                            println("^^^ Warning!")
+                            onResetTap()
+                        }
+                    )
+                }
+        ) {
+            Icon(Icons.Default.Refresh, contentDescription = "Reset", modifier = Modifier
+                .size(36.dp)
+                .align(Alignment.Center))
         }
-        IconButton(onClick = { navController.navigate(NavigationTarget.Settings.route) }) {
-            Icon(Icons.Default.Settings, contentDescription = "Settings")
+        IconButton(
+            onClick = { navController.navigate(NavigationTarget.Settings.route) },
+            modifier = Modifier.weight(1f).align(Alignment.CenterVertically)
+        ) {
+            Icon(Icons.Default.Settings, contentDescription = "Settings", modifier = Modifier.size(36.dp))
         }
     }
 }
--- app/src/main/java/com/tibadov/lifecrafter/MainScreen.kt
+++ app/src/main/java/com/tibadov/lifecrafter/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.tibadov.lifecrafter
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,7 +8,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -20,21 +25,43 @@ import androidx.navigation.NavController
 fun MainScreen(navController: NavController, modifier: Modifier = Modifier) {
     val count1 = remember { mutableStateOf(20) }
     val count2 = remember { mutableStateOf(20) }
+    val showSnackbar = remember { mutableStateOf(false) }
 
-    Column(modifier = modifier.fillMaxSize()) {
-        Box(Modifier.weight(1f)) {
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    Box(modifier = modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {
             LifeCounterWithLog(
                 count = count1, modifier = Modifier
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
                     .rotate(180f)
             )
-        }
-        ControlPanel(navController = navController, modifier = Modifier.align(Alignment.CenterHorizontally))
-        Box(Modifier.weight(1f)) {
+            Spacer(modifier = Modifier.weight(1f))
+            ControlPanel(navController = navController, modifier = Modifier.align(Alignment.CenterHorizontally), onResetTap = {
+                showSnackbar.value = true
+            })
+            Spacer(modifier = Modifier.weight(1f))
             LifeCounterWithLog(
-                count = count2
+                count = count2, modifier = Modifier
+                    .fillMaxWidth()
             )
+        }
+
+        SnackbarHost(
+            modifier = Modifier.align(Alignment.TopCenter),
+            hostState = snackbarHostState
+        )
+    }
+
+    LaunchedEffect(showSnackbar.value) {
+        if (showSnackbar.value) {
+            snackbarHostState.showSnackbar("Long press to reset")
+            showSnackbar.value = false
         }
     }
 }
+
+
 
 
