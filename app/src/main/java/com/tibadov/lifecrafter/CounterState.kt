9cde7a25a601aecb9c9fd9e0fd11ee8a7c9c2a12
Timur Ibadov <ibadov.timur@gmail.com>
2023-05-10 21:39:00 +0300

    temp9

--- app/src/main/java/com/tibadov/lifecrafter/CounterState.kt
+++ app/src/main/java/com/tibadov/lifecrafter/CounterState.kt
@@ -1,10 +1,22 @@
 package com.tibadov.lifecrafter
 
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 
-class CounterState {
-    var count = mutableStateOf(20)
-    val countLog = mutableStateListOf(count.value)
-    var lastUpdateTimeMs = 0L
+class CounterState() {
+    var count: MutableState<Int> = mutableStateOf(0)
+    var countLog: MutableList<Int> = mutableStateListOf()
+    var lastUpdateTimeMs: Long = 0L
+
+    init {
+        reset()
+    }
+
+    fun reset() {
+        count.value = 20
+        countLog.clear()
+        countLog.add(count.value)
+        lastUpdateTimeMs = 0
+    }
 }
